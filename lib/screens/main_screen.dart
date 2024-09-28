ad271107858b4a75bf2f0ed219735167fd5d4ada
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-28 07:18:57 -0400

    Add side menu

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dashboard/widgets/side_menu_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -9,9 +10,11 @@ class MainScreen extends StatelessWidget {
       body: SafeArea(
         child: Row(
           children: [
-            Expanded(
+            const Expanded(
               flex: 2,
-              child: Container(color: Colors.red),
+              child: SizedBox(
+                child: SideMenuWidget(),
+              ),
             ),
             Expanded(
               flex: 7,
